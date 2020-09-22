54655e2237afc48f70164189b22fd72e6b180104
Alex Shih <alex.alex.shih@gmail.com>
2020-09-22 00:27:28 -0400

    added getFocusData function in sidebar and moved jquery sidebar functions back to main

--- js/components/graph/Sidebar.js
+++ js/components/graph/Sidebar.js
@@ -1,9 +1,28 @@
 import React from "react";
-// import Focus from "./Focus";
-// import * as sidebarDivs from "./sidebar/sidebar_divs.js";
-// import * as focusInfo from "./sidebar/focus_descriptions";
+import Focus from "./Focus";
 
 export default class Sidebar extends React.Component {
+  getFocusData() {
+    const computerScienceFocusData = [
+      ["sci", "Scientific Computing"],
+      ["AI", "Artificial Intelligence"],
+      ["NLP", "Natural Language Processing"],
+      ["vision", "Computer Vision"],
+      ["systems", "Computer Systems"],
+      ["game", "Video Games"],
+      ["HCI", "Human Computer Interaction"],
+      ["theory", "Theory of Computation"],
+      ["web", "Web Technologies"],
+    ];
+    const focusComponents = [];
+    computerScienceFocusData.forEach((focus, i) => {
+      focusComponents.push(
+        <Focus key={i} pId={focus[0]} focusName={focus[1]} />
+      )
+    })
+    return focusComponents;
+  }
+
   render() {
     return (
       <div>
@@ -16,14 +35,17 @@ export default class Sidebar extends React.Component {
             <ul>
               <li id="graphs-nav">
                 <a href="">Graphs</a>
+              </li>
+              <li id="focuses-nav">
                 <a href="">Focuses</a>
               </li>
             </ul>
           </nav>
-          <div id="focuses">
 
+          <div id="focuses">
+            {this.getFocusData()}
           </div>
-          <div id="graphs" />
+          <div id="graphs"></div>
         </div>
         
         <div id="sidebar-button">
@@ -33,51 +55,3 @@ export default class Sidebar extends React.Component {
     )
   }
 }
-
-// Set focus button onclicks
-// $(".focus").click((event) => {
-//   var id = $(event.target).attr("id");
-//   var focusDetails = $("#" + id + "-details");
-//   if (graphComponent.state.highlightedNodes == focusInfo[id + "FocusList"]) {
-//     graphComponent.setState({ highlightedNodes: [] });
-//     focusDetails.animate({ height: "2px" }, "fast");
-//   } else {
-//     $(".details").css("height", "2px");
-//     focusDetails.animate({ height: "128px" }, "fast");
-//     focusDetails.html(focusInfo[id + "Description"]);
-//     graphComponent.setState({
-//       highlightedNodes: focusInfo[id + "FocusList"]
-//     });
-//   }
-// });
-
-// Sidebar initialization.
-// TODO: move sidebar into its own React component.
-//   $("#reset").click(function() {
-//     graphComponent.reset();
-//   });
-
-//   $(document).ready(function() {
-//     $("#nav-export").click(function() {
-//       graphComponent.openExportModal();
-//     });
-//   });
-
-//   $.ajax({
-//     url: "graphs",
-//     dataType: "json",
-//     success: function(data) {
-//       sidebarDivs.createGraphButtons(data);
-//       $(".graph-button").click(function() {
-//         var id = $(this).data("id");
-//         var name = $(this).text();
-//         graphComponent.getGraph(name);
-//         sidebarDivs.changeFocusEnable(id);
-//       });
-//     },
-//     error: function() {
-//       throw "No graphs in database";
-//     }
-//   });
-
-//   sidebarDivs.activateSidebar();
--- js/components/graph/main.js
+++ js/components/graph/main.js
@@ -2,19 +2,61 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Graph from "./Graph";
 import Container from "./Container";
+import * as focusInfo from "./sidebar/focus_descriptions";
+import * as sidebarDivs from "./sidebar/sidebar_divs.js";
 
 // The "main"
 document.addEventListener("DOMContentLoaded", () => {
-  renderReactContainer("container");
-  renderReactGraph("react-graph");
-});
+  ReactDOM.render(<Container />, document.getElementById("container"));
+  const graphComponent = renderReactGraph("react-graph");
 
-export function renderReactContainer(graph_container_id) {
-  return ReactDOM.render(
-    <Container />,
-    document.getElementById(graph_container_id)
-  );
-}
+  $("#reset").click(function() {
+    graphComponent.reset();
+  });
+
+  $(document).ready(function() {
+    $("#nav-export").click(function() {
+      graphComponent.openExportModal();
+    });
+  });
+
+  // Sends an ajax request to retrieve data for graph information
+  $.ajax({
+    url: "graphs",
+    dataType: "json",
+    success: function(data) {
+      sidebarDivs.createGraphButtons(data);
+      $(".graph-button").click(function() {
+        var id = $(this).data("id");
+        var name = $(this).text();
+        graphComponent.getGraph(name);
+        sidebarDivs.changeFocusEnable(id);
+      });
+    },
+    error: function() {
+      throw "No graphs in database";
+    }
+  });
+
+  sidebarDivs.activateSidebar();
+
+  // Set focus button onclicks
+  $(".focus").click((event) => {
+    var id = $(event.target).attr("id");
+    var focusDetails = $("#" + id + "-details");
+    if (graphComponent.state.highlightedNodes == focusInfo[id + "FocusList"]) {
+      graphComponent.setState({ highlightedNodes: [] });
+      focusDetails.animate({ height: "2px" }, "fast");
+    } else {
+      $(".details").css("height", "2px");
+      focusDetails.animate({ height: "128px" }, "fast");
+      focusDetails.html(focusInfo[id + "Description"]);
+      graphComponent.setState({
+        highlightedNodes: focusInfo[id + "FocusList"]
+      });
+    }
+  });
+});
 
 export function renderReactGraph(graph_container_id, start_blank, edit) {
   if (start_blank === undefined) {
